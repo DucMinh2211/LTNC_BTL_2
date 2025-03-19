1daa8e61e6d50b7779920786d1fb534644b88da7
gslay <mha220871@gmail.com>
2025-03-19 07:28:50 +0000

    implement StockAlertView.java  (1st)

--- Initial Code/src/main/java/com/myproject/StockAlertView.java
+++ Initial Code/src/main/java/com/myproject/StockAlertView.java
@@ -10,20 +10,33 @@ public class StockAlertView implements StockViewer {
 
     public StockAlertView(double highThreshold, double lowThreshold) {
         // TODO: Implement constructor
+        this.alertThresholdHigh = highThreshold;
+        this.alertThresholdLow = lowThreshold;
     }
 
     @Override
     public void onUpdate(StockPrice stockPrice) {
         // TODO: Implement alert logic based on threshold conditions
+        String code = stockPrice.getCode();
+        double currentPrice = stockPrice.getAvgPrice();
+        Double lastAlertedPrice = lastAlertedPrices.get(code);
+
+        if (lastAlertedPrice == currentPrice) return;
+        if (currentPrice < alertThresholdLow) {
+            alertBelow(code, currentPrice);
+        }
+        else if (currentPrice > alertThresholdHigh) {
+            alertAbove(code, currentPrice);
+        }
     }
 
     private void alertAbove(String stockCode, double price) {
         // TODO: Call Logger to log the alert
-        Logger.notImplementedYet("alertAbove");
+        Logger.logAlert(stockCode, price);
     }
 
     private void alertBelow(String stockCode, double price) {
         // TODO: Call Logger to log the alert
-        Logger.notImplementedYet("alertBelow");
+        Logger.logAlert(stockCode, price);
     }
 }
