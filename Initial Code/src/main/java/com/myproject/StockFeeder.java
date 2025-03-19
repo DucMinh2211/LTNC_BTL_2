b8873a88d8c98fd1483a254e19bdf37d9a7f2d6a
gslay <mha220871@gmail.com>
2025-03-19 06:40:12 +0000

    implement StockFeeder.java (1st)

--- Initial Code/src/main/java/com/myproject/StockFeeder.java
+++ Initial Code/src/main/java/com/myproject/StockFeeder.java
@@ -12,22 +12,43 @@ public class StockFeeder {
 
     public static StockFeeder getInstance() {
         // TODO: Implement Singleton logic
-        return null;
+        if (instance == null) {
+            instance = new StockFeeder();
+        }
+        return instance;
     }
 
     public void addStock(Stock stock) {
         // TODO: Implement adding a stock to stockList
+        stockList.add(stock);
+        viewers.put(stock.getCode(), new ArrayList<> ());
     }
 
     public void registerViewer(String code, StockViewer stockViewer) {
         // TODO: Implement registration logic, including checking stock existence
+        if (!viewers.containsKey(code)) {
+            Logger.errorRegister(code);
+        }
+        else {
+            viewers.get(code).add(stockViewer);
+        }
     }    
 
     public void unregisterViewer(String code, StockViewer stockViewer) {
         // TODO: Implement unregister logic, including error logging
+        bool viewerRegistered = viewers.get(code).contains(stockViewer);
+        if (!viewers.containsKey(code) || !viewerRegistered) {
+            Logger.errorUnregister(code);
+        }
+        else {
+            viewers.get(code).remove(stockViewer);
+        }
     }
 
     public void notify(StockPrice stockPrice) {
         // TODO: Implement notifying registered viewers about price updates
+        for (StockViewer stockViewer : viewers.get(stockPrice.getCode())) {
+            stockViewer.onUpdate(stockPrice);
+        }
     }
 }
